e6e864bd0bc54910858d787ad89da284833e22b2
Gabo <GaboEI@users.noreply.github.com>
2026-07-21 04:41:04 -0500

    fix debian amneziawg guidance

--- distros/debian.sh
+++ distros/debian.sh
@@ -11,10 +11,11 @@ DISTRO_DNS_PACKAGES=(dnsutils)
 DISTRO_PYTHON_CRYPTOGRAPHY_PACKAGE="python3-cryptography"
 DISTRO_POLKIT_PACKAGE="polkitd"
 DISTRO_AMNEZIAWG_GUIDANCE_COMMANDS=(
-  "sudo apt install -y software-properties-common python3-launchpadlib gnupg2 linux-headers-\$(uname -r)"
-  "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 57290828"
-  "echo 'deb https://ppa.launchpadcontent.net/amnezia/ppa/ubuntu focal main' | sudo tee -a /etc/apt/sources.list"
-  "echo 'deb-src https://ppa.launchpadcontent.net/amnezia/ppa/ubuntu focal main' | sudo tee -a /etc/apt/sources.list"
+  "sudo apt install -y python3-launchpadlib gnupg2 linux-headers-\$(uname -r)"
+  "tmpdir=\$(mktemp -d) && GNUPGHOME=\"\$tmpdir\" gpg --batch --keyserver keyserver.ubuntu.com --recv-keys 57290828 && GNUPGHOME=\"\$tmpdir\" gpg --batch --export 57290828 | sudo gpg --dearmor --yes -o /usr/share/keyrings/amneziawg-archive-keyring.gpg; rm -rf \"\$tmpdir\""
+  "sudo chmod 0644 /usr/share/keyrings/amneziawg-archive-keyring.gpg"
+  "echo 'deb [signed-by=/usr/share/keyrings/amneziawg-archive-keyring.gpg] https://ppa.launchpadcontent.net/amnezia/ppa/ubuntu focal main' | sudo tee /etc/apt/sources.list.d/amneziawg-ppa.list"
+  "echo 'deb-src [signed-by=/usr/share/keyrings/amneziawg-archive-keyring.gpg] https://ppa.launchpadcontent.net/amnezia/ppa/ubuntu focal main' | sudo tee -a /etc/apt/sources.list.d/amneziawg-ppa.list"
   "sudo apt-get update"
   "sudo apt-get install -y amneziawg"
 )
